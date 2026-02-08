30888d794e431d80fa277f9568cfce0f1bd61f39
sribich <8329153+sribich@users.noreply.github.com>
2026-02-08 15:06:22 -0500

    chore(fude): remove InlineMenu

--- project/fude/api/typescript/fude/src/components/InlineMenu/menu/context/OrderedMenuContext.ts
+++
@@ -1,8 +0,0 @@
-/* import { createContext, Dispatch, SetStateAction } from "react"
-
-type OrderedMenuContext = {
-    setItemIds: Dispatch<SetStateAction<string[]>>
-}
-
-export const OrderedMenuContext = createContext<OrderedMenuContext>(undefined)
- */
